db530507399fd620564ea6b9f4a85a448ad5d8f9
David Holeman <david.holeman@opexnetworks.com>
2024-07-15 17:27:47 -0700

    save

--- Globulon/Functions/deleteTripGPSJournalEntry.swift
+++ Globulon/Functions/deleteTripGPSJournalEntry.swift
@@ -10,7 +10,7 @@
 /// Display trip details
 ///
 /// # Version History
-/// ### 0.1.0.64
+/// ### 0.1.0.65
 /// # - Fixed to delete the GPS entry instead of the trip
 /// # - *Date*: 07/15/24
 
@@ -56,54 +56,3 @@ func deleteTripGPSJournalEntry(tripTimestamp: Date, journalEntryTimestamp: Date)
     
     return result
 }
-
-
-//func deleteTripGPSJournalEntry(tripTimestamp: Date, journalEntryTimestamp: Date) -> Bool {
-//    
-//    LogEvent.print(module: "deleteTripJournalEntry()", message: "Delete trip journal entry ...")
-//    
-//    let context = ModelContext(AppEnvironment.sharedModelContainer)
-//    
-//    var result = false
-//    
-//    do {
-//        
-//        /// Define a fetch descriptor that sorts the entries by timestamp in ascending order
-//        let fetchDescriptor = FetchDescriptor<TripSummariesSD>(sortBy: [SortDescriptor(\TripSummariesSD.originationTimestamp, order: .forward)])
-//        
-//        /// Fetch all entries from the context
-//        let tripSummariesSD = try context.fetch(fetchDescriptor)
-//        
-//        /// Find the first trip that matches the tripTimestamp
-//        var firstFilteredTrip: TripSummariesSD? {
-//            return tripSummariesSD.first { tripSummary in
-//                return tripSummary.originationTimestamp == tripTimestamp
-//            }
-//        }
-//        
-//        var index = 0
-//        while index < (firstFilteredTrip?.toTripJournal!.count)! {
-//            if firstFilteredTrip?.toTripJournal![index].timestamp == journalEntryTimestamp {
-//                context.delete(firstFilteredTrip!.toTripJournal![index])
-//                break
-//            }
-//            index += 1
-//        }
-//        
-//        context.delete(firstFilteredTrip!)
-//        
-//        do {
-//            try context.save()
-//            result = true
-//            print("** Deleted trip item dated \(firstFilteredTrip?.originationTimestamp ?? Date())")
-//            LogEvent.print(module: "deleteTripJournalEntry()", message: "Deleted trip item dated \(firstFilteredTrip?.originationTimestamp ?? Date())")
-//        } catch {
-//            // Handle the error, e.g., show an alert to the user
-//            LogEvent.print(module: "deleteTripJournalEntry()", message: "Error saving context after deleting a trip item: \(error)")
-//        }
-//    } catch {
-//        LogEvent.print(module: "deleteTripSData()", message: "An error occurred: \(error)")
-//    }
-//    
-//    return result
-//}
